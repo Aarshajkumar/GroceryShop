02008e313bf811bab76663c6cc3430e868a56af6
Dell <Dell@192.168.1.33>
2025-09-24 13:24:14 +0530

    Adminpage commit

--- GroceryApplication/src/main/java/pages/AdminPage.java
+++ GroceryApplication/src/main/java/pages/AdminPage.java
@@ -3,13 +3,16 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 
 public class AdminPage {
 public WebDriver driver;
 public AdminPage(WebDriver driver) {
-	this.driver =driver;
+this.driver = driver;
 }
+
+@FindBy(xpath ="//a[@href='https://groceryapp.uniqassosiates.com/admin/list-admin' and @class='small-box-footer']")WebElement adminmoreInfo;
 public void clickMoreInfo() {
 	WebElement adminmoreInfo = driver.findElement(By.xpath("//a[@href='https://groceryapp.uniqassosiates.com/admin/list-admin' and @class='small-box-footer'] "));
 	adminmoreInfo.click();
--- GroceryApplication/src/main/java/pages/HomePage.java
+++ GroceryApplication/src/main/java/pages/HomePage.java
@@ -3,14 +3,16 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePage {
 public WebDriver driver;
 public HomePage(WebDriver driver) {
 	this.driver = driver;
 }
+@FindBy(xpath ="//img[@src='https://groceryapp.uniqassosiates.com/public/assets/admin/dist/img/avatar5.png']")WebElement loginclick;
 public void loginclick() {
-	WebElement loginclick= driver.findElement(By.xpath("//img[@src='https://groceryapp.uniqassosiates.com/public/assets/admin/dist/img/avatar5.png']"));
+	//WebElement loginclick= driver.findElement(By.xpath("//img[@src='https://groceryapp.uniqassosiates.com/public/assets/admin/dist/img/avatar5.png']"));
 	loginclick.click();
 }
 public void logoutclick() {
